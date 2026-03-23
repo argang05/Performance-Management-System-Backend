3758466f3455510223be6db8a661fef828b3f889
Abhirup Ganguly <abhirup@Abhirups-MacBook-Air.local>
2026-03-24 00:17:27 +0530

    minor fix 4

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
 
 # Application definition
 
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def healthz(request):
+    return HttpResponse("ok", content_type="text/plain")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("healthz/", healthz),
     path('api/accounts/', include('accounts.urls')),
     path('api/employees/', include('employees.urls')),
     path('api/evaluations/', include('evaluations.urls')),
